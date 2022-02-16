46b6d17d3cf87df970104aedc5dbe278f30d9cc2
thangasblockchain <thangarajm@icloud.com>
2022-02-16 19:16:48 +0530

    changing components to jsx and understanding the flow, children

--- adopt-me/src/app.js
+++ adopt-me/src/app.js
@@ -2,27 +2,49 @@
 //* global React ReactDOM */
 
 
-import React from "react";
+//import React from "react";
 import  ReactDOM  from "react-dom";
 import Pet from "./pet";
 
 
-const App = () => {
-  // this is an app component
-  // just like rubber stamp
-  // anywhere we want to create app component, we can use it like a rubber stamp to stamp it down
-  // we have not used this component yet
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "my-store" }, "Adopt me!"),
-    React.createElement(Pet, { name: "kunal", animal: "dog", breed: "husky" }),
-    React.createElement(Pet, {
-      name: "kamal",
-      animal: "dog",
-      breed: "havanese",
-    }),
-    React.createElement(Pet, { name: "haran", animal: "cow", breed: "indian" }),
-  ]);
-};
+// const App = () => {
+//   // this is an app component
+//   // just like rubber stamp
+//   // anywhere we want to create app component, we can use it like a rubber stamp to stamp it down
+//   // we have not used this component yet
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", { id: "my-store" }, "Adopt me!"),
+//     React.createElement(Pet, { name: "kunal", animal: "dog", breed: "husky" }),
+//     React.createElement(Pet, {
+//       name: "kamal",
+//       animal: "dog",
+//       breed: "havanese",
+//     }),
+//     React.createElement(Pet, { name: "haran", animal: "cow", breed: "indian" }),
+//   ]);
+// };
 // below is creating the instance of component
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
-ReactDOM.render(React.createElement(App), document.getElementById("root1"));
+// ReactDOM.render(React.createElement(App), document.getElementById("root"));
+// ReactDOM.render(React.createElement(App), document.getElementById("root1"));
+
+
+
+
+// rewriting the above app component in JSX
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt me !!!!</h1>
+      <Pet name = "Luna" animal = "dog" breed = "husky">fun and working</Pet>
+      <Pet name = "Kunal" animal = "Cat" breed = "Mewo" />
+      <Pet name = "Haran" animal = "Cow" breed = "Holy"> i am the children</Pet>
+      <h2 >below is place using you can add input</h2>
+      <input />
+    </div>
+  )
+}
+
+//replace it to app component after making all the changes
+ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root1"));
--- adopt-me/src/pet.js
+++ adopt-me/src/pet.js
@@ -1,15 +1,31 @@
 // declaring an array of function
 // creating a pet component
 
-import React from "react";
-const Pet = (props) => {
-    return React.createElement("div", {}, [
-      // below is array of components to render
-      React.createElement("div", {}, "Cat"),
-      React.createElement("h2", {}, props.name),
-      React.createElement("h3", {}, props.animal),
-      React.createElement("h3", {}, props.breed),
-    ]);
-  };
+//import React from "react";
+
+// const Pet = (props) => {
+//     return React.createElement("div", {}, [
+//       // below is array of components to render
+//       React.createElement("div", {}, "Cat"),
+//       React.createElement("h2", {}, props.name),
+//       React.createElement("h3", {}, props.animal),
+//       React.createElement("h3", {}, props.breed),
+//     ]);
+//   };
+
+  // re writing the pet component using jsx
+
+  const Pet = (props) => {
+
+    return(
+        <div>{props.children}
+            <h2>{props.name}</h2>
+            <h3>{props.animal}</h3>
+            <h3>{props.breed}</h3>
+        </div>
+    )
+
+  }
+
 
   export default Pet;
