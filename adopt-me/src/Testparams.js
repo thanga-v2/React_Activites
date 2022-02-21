40c393d1b4dfbb2309840c5dab96b9f45c190e48
thangasblockchain <thangarajm@icloud.com>
2022-02-21 13:41:33 +0530

    test

--- adopt-me/src/Testparams.js
+++ adopt-me/src/Testparams.js
@@ -4,12 +4,15 @@ import { useState } from "react"
 const TestParams = () => {
     // adding the hook
     const[userstate,SetUserstate] = useState("User-Blockchain")
+    const[testStata,setTestState] = useState("test")
     console.log("user - ",userstate);
+    console.log("second state -",testStata);
     return (
         <div>
             <label htmlFor="userstate">
             <h1> rubber stamp</h1>
         <input id="testing" onChange={e => SetUserstate(e.target.value)} value={userstate} placeholder="testing rubber stamp"/> 
+        <input id="testing now" value={testStata} onChange={e => setTestState(e.target.value)}/>
         </label>
 
     </div>
